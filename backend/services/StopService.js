ded98ea3dd60746ee23204d929247b5c67696be9
fuxuras <furkancanokal1@gmail.com>
2025-02-20 16:10:20 +0300

    feat: stop service crud

--- backend/services/StopService.js
+++ backend/services/StopService.js
@@ -25,6 +25,74 @@ const stopService = {
             console.error(error);
             res.status(500).json({ error: "Server Error" });
           }
+    },
+    getStopById: async (req, res) => {
+      try {
+        const { stop_id } = req.params;
+        const [rows] = await pool.execute(
+          `SELECT * FROM stops
+            WHERE stop.stop_id = ?`,
+          [stop_id]
+        );
+        res.json(rows);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server Error" })
+      }
+    },
+    deleteStopById: async (req,res) =>{
+      try {
+        const { stop_id } = req.params;
+        await pool.execute(`
+          DELETE FROM stops
+          WHERE stop_id = ?
+          `, [stop_id])
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "server error" })
+      }
+    },
+    updateStop: async (req,res) => {
+      try {
+        const {stop_id, stop_code, stop_name, stop_desc, stop_lat, stop_lon, zone_id, stop_url, location_type, parent_station, stop_timezone, wheelchair_boarding} = req.body;
+        const query = `
+          UPDATE stops
+          SET
+            stop_code = ?,
+            stop_name = ?,
+            stop_desc = ?,
+            stop_lat = ?,
+            stop_lon = ?,
+            zone_id = ?,
+            stop_url = ?,
+            location_type = ?,
+            parent_station = ?,
+            stop_timezone = ?,
+            wheelchair_boarding = ?
+          WHERE stop_id = ? 
+        `;
+        const result = await pool.execute(query, [stop_code, stop_name, stop_desc, stop_lat, stop_lon, zone_id, stop_url, location_type, parent_station, stop_timezone, wheelchair_boarding, stop_id]);
+        if(result.affectedRows == 0){
+          return res.status(404).json({ error: "stop not found" });
+        }
+        res.status(200).json({ message: "stop successfuly updated"});
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({error: "server error"});
+      }
+    },
+    saveStop: async (req,res) => {
+      try {
+        const {stop_code, stop_name, stop_desc, stop_lat, stop_lon, zone_id, stop_url, location_type, parent_station, stop_timezone, wheelchair_boarding} = req.body;
+        const query = `
+        INSERT INTO stops(stop_code, stop_name, stop_desc, stop_lat, stop_lon, zone_id, stop_url, location_type, parent_station, stop_timezone, wheelchair_boarding)
+        VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+        const [result] = await pool.execute(query, [stop_code, stop_name, stop_desc, stop_lat, stop_lon, zone_id, stop_url, location_type, parent_station, stop_timezone, wheelchair_boarding]);
+        res.status(201).json({message: "stop saved succesfully", stop_id: result.insertId});
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({error: "server errro"});
+      }
     }
     
 };
