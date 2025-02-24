a01f5e025fc5cbee26dff6f1e2b128ab1ea795d9
fuxuras <furkancanokal1@gmail.com>
2025-02-24 15:26:52 +0300

    feat: index

--- backend/controllers/IndexController.js
+++ backend/controllers/IndexController.js
@@ -2,13 +2,21 @@ const express = require("express");
 const router = express.Router();
 
 // import routers
+const authController = require("./AuthController.js");
+const calendarController = require("./CalendarController.js");
 const routeController = require("./RouteController.js");
 const stopController = require("./StopController.js");
-const authController = require("./AuthController.js");
+const stopTimeController = require("./StopTimeController.js");
+const tripController = require("./TripController.js")
+const projectController = require("./ProjectController.js")
 
 // routes
+router.use("/auth", authController);
+router.use("/calendars", calendarController);
 router.use("/routes",routeController);
 router.use("/stops", stopController);
-router.use("/auth", authController);
+router.use("/stop-times", stopTimeController);
+router.use("/trips", tripController)
+router.use("/projets", projectController);
 
 module.exports = router;
