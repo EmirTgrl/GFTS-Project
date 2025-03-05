fcc9224b395a111331f22f0131513f0e41d33127
fuxuras <furkancanokal1@gmail.com>
2025-03-05 15:32:05 +0300

    feat: route service update and save are dynamic now

--- backend/services/RouteService.js
+++ backend/services/RouteService.js
@@ -53,47 +53,41 @@ const routeService = {
   updateRoute: async (req, res) => {
     try {
       const user_id = req.user.id;
-      const {
-        route_id,
-        project_id,
-        agency_id,
-        route_short_name,
-        route_long_name,
-        route_desc,
-        route_type,
-        route_url,
-        route_color,
-        route_text_color,
-        route_sort_order,
-      } = req.body;
+      const validFields = [
+        "route_id",
+        "project_id",
+        "agency_id",
+        "route_short_name",
+        "route_long_name",
+        "route_desc",
+        "route_type",
+        "route_url",
+        "route_color",
+        "route_text_color",
+        "route_sort_order"
+      ];
+      const { route_id, ...params } = req.body;
+      
+      const fields = [];
+      const values = [];
+
+      for (const param in params) {
+        if (validFields.includes(param)) {
+          fields.push(`${param} = ?`);
+          values.push(params[param]);
+        } else {
+          console.warn(`unexpected field ${param}`);
+        }
+      }
+      
       const query = `
         UPDATE routes
         SET
-          agency_id = ?, 
-          route_short_name = ?, 
-          route_long_name = ?, 
-          route_desc = ?, 
-          route_type = ?, 
-          route_url = ?, 
-          route_color = ?, 
-          route_text_color = ?, 
-          route_sort_order = ?,
-        WHERE route_id = ? AND user_id = ? AND project_id = ?
+          ${fields.includes(param)}
+        WHERE route_id = ? AND user_id = ?
       `;
-      const [result] = await pool.execute(query, [
-        agency_id,
-        route_short_name,
-        route_long_name,
-        route_desc,
-        route_type,
-        route_url,
-        route_color,
-        route_text_color,
-        route_sort_order,
-        route_id,
-        user_id,
-        project_id,
-      ]);
+      const [result] = await pool.execute(query, [...values, route_id, user_id]);
+      
       if (result.affectedRows === 0) {
         return res.status(404).json({ error: "Route not found" });
       }
@@ -106,47 +100,48 @@ const routeService = {
   saveRoute: async (req, res) => {
     try {
       const user_id = req.user.id;
-      const {
-        agency_id,
-        project_id,
-        route_short_name,
-        route_long_name,
-        route_desc,
-        route_type,
-        route_url,
-        route_color,
-        route_text_color,
-        route_sort_order,
-      } = req.body;
+      const validFields = [
+        "project_id",
+        "agency_id",
+        "route_short_name",
+        "route_long_name",
+        "route_desc",
+        "route_type",
+        "route_url",
+        "route_color",
+        "route_text_color",
+        "route_sort_order"
+      ];
+      const { ...params} = req.body;
 
-      const [rows] = await pool.execute(
-        "SELECT MAX(CAST(route_id AS UNSIGNED)) as max_id FROM routes WHERE project_id = ? AND user_id = ?",
-        [project_id, user_id]
-      );
-      const lastId = rows[0].max_id || 0;
-      const route_id = String(parseInt(lastId, 10) + 1).padStart(5, "0");
+      const fields = [];
+      const values = [];
+      const placeholders = [];
+
+      for (const param in params) {
+        if (validFields.includes(param)) {
+          fields.push(param);
+          values.push(params[param]);
+          placeholders.push("?")
+        } else {
+          console.warn(`unexpected field in ${param}`);
+        }
+      }
+
+      fields.push("user_id");
+      placeholders.push("?");
+      values.push(user_id);
 
       const query = `
-        INSERT INTO routes(route_id, user_id, agency_id, project_id, route_short_name, route_long_name, route_desc, route_type, route_url, route_color, route_text_color, route_sort_order)
-        VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        INSERT INTO routes(${fields.join(", ")})
+        VALUES(${placeholders.join(", ")})
       `;
-      const [result] = await pool.execute(query, [
-        route_id,
-        user_id,
-        agency_id,
-        project_id,
-        route_short_name,
-        route_long_name,
-        route_desc,
-        route_type,
-        route_url,
-        route_color,
-        route_text_color,
-        route_sort_order,
-      ]);
+
+      const [result] = await pool.execute(query, values);
+
       res.status(201).json({
         message: "Route saved successfully",
-        route_id: route_id,
+        route_id: result.insertId,
       });
     } catch (error) {
       console.error(error);
