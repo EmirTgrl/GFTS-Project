902eceec9247122f3d7023ca44e3420b44a0b4ab
Emir Tuğrul Aytekin <emirtgrl1106@gmail.com>
2025-06-08 10:41:27 +0300

    update server port configuration

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const { checkDatabaseConnection } = require("./db.js");
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
